702f40a068e5b84a09e9ddd248ba3c7603ef1e58
James OHea <james.ohea@diamond.ac.uk>
2010-03-09 16:57:17 +0000

    Added image processing header file and driver
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/support/firewireDCAM@42120 e099a375-04f9-0310-9d5f-a741eaff62e1

---
+++ firewireDCAMApp/src/drvImgPro.c
@@ -0,0 +1,40 @@
+/* For exporting the configuration functions to the ioc shell */
+
+#include <iocsh.h>
+#include <drvSup.h>
+#include <epicsExport.h>
+
+#include "imgPro.h"
+
+/* EPICS iocsh shell commands */
+
+static const iocshArg imgProConfigArg0 = {"Port name", iocshArgString};
+static const iocshArg imgProConfigArg1 = {"Frame Queue Size", iocshArgString};
+static const iocshArg imgProConfigArg2 = {"Blocking Callbacks", iocshArgInt};
+static const iocshArg imgProConfigArg3 = {"NDArray Port", iocshArgInt};
+static const iocshArg imgProConfigArg4 = {"NDArray Address", iocshArgInt};
+static const iocshArg imgProConfigArg5 = {"Max Buffers", iocshArgInt};
+static const iocshArg imgProConfigArg6 = {"Max Memory", iocshArgInt};
+static const iocshArg * const imgProConfigArgs[] =  {&imgProConfigArg0,
+                                                     &imgProConfigArg1,
+                                                     &imgProConfigArg2,
+                                                     &imgProConfigArg3,
+                                                     &imgProConfigArg4,
+                                                     &imgProConfigArg5,
+                                                     &imgProConfigArg6};
+
+static const iocshFuncDef imgProFuncDef = {"imgProConfigure", 7, imgProConfigArgs};
+
+static void imgProCallFunc(const iocshArgBuf *args)
+{
+    imgProConfigure(args[0].sval, args[1].sval, args[2].ival, args[3].ival, args[4].ival, args[5].ival, args[6].ival);
+}
+
+/* Register imgPro::imgProConfigure for use on iocsh */
+
+void imgProRegister(void)
+{
+    iocshRegister(&imgProFuncDef, imgProCallFunc);
+}
+
+epicsExportRegistrar(imgProRegister);
