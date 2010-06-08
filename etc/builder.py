e577c32ab5b59964dafa1c8ad0ebc2281b67b54e
James OHea <james.ohea@diamond.ac.uk>
2010-06-08 09:14:38 +0000

    Commiting latest changes to firewireDCAM - including removing of imgPro files and references and references to R3.14.10
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/support/firewireDCAM@46022 e099a375-04f9-0310-9d5f-a741eaff62e1

--- etc/builder.py
+++ etc/builder.py
@@ -5,53 +5,45 @@ from iocbuilder.modules.areaDetector import AreaDetector, ADBase, simDetector
 from iocbuilder.modules.dc1394 import Dc1394
 
 class _firewireDCAM(AutoSubstitution):
-    TemplateFile="firewireDCAM.db"
+    TemplateFile="firewireDCAM.template"
+    SubstitutionOverwrites = [_ADBase]
 
-class firewireDCAM(Device):
-    '''Creates a firewire DCAM camera'''
-    Dependencies = (Dc1394,)
-    
-    def __init__(self, ID, MEMORY, BUFFERS = 16, SPEED = 800, COLOUR = 0, **args):  
-        # Pass the arguments to the relevant templates
-        self.template = _firewireDCAM(**filter_dict(args, _firewireDCAM.ArgInfo.Names()))
-        self.base = ADBase(**filter_dict(args, ADBase.ArgInfo.Names()))        
+class firewireDCAM(ADBase):
+    '''Creates a firewireDCAM camera areaDetector driver'''
+    gda_template = "firewireDCAM"
+    Dependencies = (Dc1394,)    
+    def __init__(self, ID, SPEED = 800, COLOUR = 0, BUFFERS = 50, MEMORY = -1, **args):
         # Init the superclass
-        self.__super.__init__()
+        self.__super.__init__(**filter_dict(args, ADBase.ArgInfo.Names()))
+        # Init the firewireDCAM class
+        self.template = _firewireDCAM(**filter_dict(args, _firewireDCAM.ArgInfo.Names()))
         # Store the args
-        self.__dict__.update(self.base.args)        
         self.__dict__.update(self.template.args)
         self.__dict__.update(locals())
 
-    # __init__ arguments        
-    ArgInfo = _firewireDCAM.ArgInfo + \
-        ADBase.ArgInfo.filtered(without = _firewireDCAM.ArgInfo.Names()) + \
-               makeArgInfo(__init__,
+    # __init__ arguments
+    ArgInfo = ADBase.ArgInfo + _firewireDCAM.ArgInfo + makeArgInfo(__init__,
         ID     = Simple('Cam ID with 0x prefix', str),
-        MEMORY = Simple(
-            'Max memory to allocate, should be maxw*maxh*nbuffer for driver'
-            ' and all attached plugins', int),        
-        BUFFERS = Simple(
-            'Maximum number of NDArray buffers to be created for plugin'
-            ' callbacks', int),        
         SPEED  = Choice('Bus speed', [400, 800]),
         COLOUR = Enum  ('Colour mode', ['B+W', 'Col'])) 
-        
+        BUFFERS = Simple('Maximum number of NDArray buffers to be created for '
+            'plugin callbacks', int),
+        MEMORY  = Simple('Max memory to allocate, should be maxw*maxh*nbuffer '
+            'for driver and all attached plugins', int))
+
     # Device attributes
     LibFileList = ['firewireDCAM']
-    DbdFileList = ['firewireDCAM'] 
+    DbdFileList = ['firewireDCAM']
 
     def InitialiseOnce(self):
         print '# Scan the firewire bus for cameras'
         print 'FDC_InitBus()'
 
     def Initialise(self):
-        # ADBase has stored these for us
         print 'FDC_Config("%(PORT)s", %(ID)s, %(SPEED)d, %(BUFFERS)d,' \
             ' %(MEMORY)d, %(COLOUR)d)' % self.__dict__
-        
-# just use a simDetector for simulations
-def firewireDCAM_sim(**args):
-    return simDetector(
-        WIDTH = 1024, HEIGHT = 768,
-        **filter_dict(args, simDetector.ArgInfo.Names()))
+
+def firewireDCAM_sim(**kwargs):
+    return simDetector(1024, 768, **kwargs)
+
 SetSimulation(firewireDCAM, firewireDCAM_sim)
